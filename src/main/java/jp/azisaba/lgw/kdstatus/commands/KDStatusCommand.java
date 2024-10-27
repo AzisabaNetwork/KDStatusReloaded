c6b423be807941ec4cfa7ff3c52a1f5b65b9df38
sysnote8main <sysnote8main@gmail.com>
2024-10-27 16:40:01 +0900

    [Add] Reconnect command and support tab-complete

--- src/main/java/jp/azisaba/lgw/kdstatus/commands/KDStatusCommand.java
+++ src/main/java/jp/azisaba/lgw/kdstatus/commands/KDStatusCommand.java
@@ -1,14 +1,21 @@
 package jp.azisaba.lgw.kdstatus.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import jp.azisaba.lgw.kdstatus.KDStatusReloaded;
 import jp.azisaba.lgw.kdstatus.utils.Chat;
 
-public class KDStatusCommand implements CommandExecutor {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class KDStatusCommand implements CommandExecutor, TabCompleter {
+    private static final List<String> modes = Arrays.asList("reload", "migration", "reconnect-db");
 
     private final KDStatusReloaded plugin;
 
@@ -18,7 +25,7 @@ public class KDStatusCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if ( args.length <= 0 ) {
+        if (args.length == 0) {
             sender.sendMessage(Chat.f("&cUsage: {0}", cmd.getUsage().replace("{LABEL}", label)));
             return true;
         }
@@ -37,7 +44,25 @@ public class KDStatusCommand implements CommandExecutor {
             return true;
         }
 
+        if ( args[0].equalsIgnoreCase("reconnect-db") ) {
+            sender.sendMessage("Reconnecting database...");
+            plugin.sql.reconnect();
+            sender.sendMessage("Reconnect was finished.");
+            sender.sendMessage("Auto-connection test is running...");
+            boolean alive = plugin.sql.isConnectionAlive();
+            sender.sendMessage("Result: Connection is " + (alive ? ChatColor.GREEN + "alive" : ChatColor.RED + "dead"));
+            return true;
+        }
+
         sender.sendMessage(Chat.f("&cUsage: {0}", cmd.getUsage().replace("{LABEL}", label)));
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
+        if(args.length == 1) {
+            return modes;
+        }
+        return Collections.emptyList();
     }
 }
